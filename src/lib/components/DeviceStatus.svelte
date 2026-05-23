814bda80410d9d9318821b1aa56a1d81113f9bbb
m.aay <no-reply@mally.qzz.io>
2026-05-23 19:58:01 +0700

    i dont know

--- src/lib/components/DeviceStatus.svelte
+++ src/lib/components/DeviceStatus.svelte
@@ -1,8 +1,11 @@
 <script>
   import { BluetoothOff, Wifi } from "@lucide/svelte";
+  import { getBluetoothStatus } from "tauri-plugin-android-utils-api";
+
+
 </script>
 
 <div class="flex flex-row gap-5">
-  <Wifi size={15}/>
+  <Wifi size={15} />
   <BluetoothOff size={15} />
 </div>
